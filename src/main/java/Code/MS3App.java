4baa9daa76055a30929a7766c90b02fcad8aa199
RuzannaG <grigoryanruz89@gmail.com>
2020-05-05 23:22:53 -0700

    test

--- src/main/java/Code/MS3App.java
+++ src/main/java/Code/MS3App.java
@@ -14,7 +14,7 @@ public class MS3App extends OtherUtils {
         public static void main(String[] args) {
         MS3App app = new MS3App(); //create instance of Code.MS3App to access the instance methods and variables
 
-            //
+            // hey
         List<String> filesToProcess =app.listNamesOfAllFiles(ConfigurationReader.get("path")); //get list of names of CSV file
         for (String fileName: filesToProcess){
             app.processCSV(ConfigurationReader.get("path"), fileName);
